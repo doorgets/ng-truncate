d823bf11a3f8d6fdb8b20d996548de64f837f5bd
Mounir R'Quiba <mounir.rquiba@gmail.com>
2017-02-26 13:33:32 +0100

    init index app

---
+++ index.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+
+import { DoorgetsTruncatePipe } from './src/ng-truncate.pipe';
+import { DoorgetsTruncateDirective } from './src/ng-truncate.directive';
+import { DoorgetsTruncateService } from './src/ng-truncate.service';
+
+export * from "./src/ng-truncate.pipe";
+export * from "./src/ng-truncate.parser";
+export * from "./src/ng-truncate.directive";
+export * from "./src/ng-truncate.service";
+
+@NgModule({
+  providers: [DoorgetsTruncateService],
+  declarations: [DoorgetsTruncatePipe, DoorgetsTruncateDirective],
+  exports: [DoorgetsTruncatePipe, DoorgetsTruncateDirective]
+})
+export class DoorgetsTruncateModule {
+}
